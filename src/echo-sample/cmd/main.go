4eae5a830891e71126406ceca55dc61b7a0adc06
lee212400 <i2124@agent-grow.com>
2025-04-04 10:20:24 +0900

    routing

--- src/echo-sample/cmd/main.go
+++ src/echo-sample/cmd/main.go
@@ -9,12 +9,23 @@ import (
 
 func main() {
 	newE := echo.New()
-	newE.GET("/user:name", func(e echo.Context) error {
+
+	routing(newE)
+
+	newE.Logger.Fatal(newE.Start(":8888"))
+}
+
+func routing(e *echo.Echo) {
+	userIdGroup := e.Group("/user") // 重複するprefixはgroup化した方が管理しやすい
+
+	// 経路とパラメータ設定
+	e.GET("/user/:name", func(e echo.Context) error {
 		name := e.Param("name")
-		return e.String(http.StatusOK, fmt.Sprintf("Hello, %s User!", &name))
+		return e.String(http.StatusOK, fmt.Sprintf("Hello, %s User!", name))
 	})
 
-	newE.POST("/user", func(e echo.Context) error {
+	// 経路設定
+	e.POST("/user", func(e echo.Context) error {
 		var reqDt any
 		if err := e.Bind(&reqDt); err != nil {
 			return e.JSON(http.StatusBadRequest, err.Error())
@@ -23,5 +34,22 @@ func main() {
 		return e.String(http.StatusOK, "User Submitted!")
 	})
 
-	newE.Logger.Fatal(newE.Start(":8888"))
+	// 経路とパラメータ設定
+	userIdGroup.PUT("/:id", func(e echo.Context) error {
+		var reqDt any
+		id := e.Param("id")
+		if err := e.Bind(&reqDt); err != nil {
+			return e.JSON(http.StatusBadRequest, err.Error())
+		}
+
+		return e.String(http.StatusOK, fmt.Sprintf("PUT, %s User!", id))
+	})
+
+	// 経路とパラメータ設定
+	userIdGroup.DELETE("/:id", func(e echo.Context) error {
+		id := e.Param("id")
+
+		return e.String(http.StatusOK, fmt.Sprintf("DELETE, %s User!", id))
+	})
+
 }
