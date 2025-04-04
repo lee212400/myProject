e7bb40413a0114bf57dfa99d991dd6f3648fd799
lee212400 <i2124@agent-grow.com>
2025-04-04 11:24:06 +0900

    request

--- src/echo-sample/cmd/main.go
+++ src/echo-sample/cmd/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	newE := echo.New()
 
-	routing(newE)
+	//routing(newE)
+	request(newE)
 
 	newE.Logger.Fatal(newE.Start(":8888"))
 }
@@ -52,4 +53,69 @@ func routing(e *echo.Echo) {
 		return e.String(http.StatusOK, fmt.Sprintf("DELETE, %s User!", id))
 	})
 
+}
+
+func request(e *echo.Echo) {
+	// 経路
+	e.GET("/user/:name", func(e echo.Context) error {
+		name := e.Param("name")
+		return e.String(http.StatusOK, fmt.Sprintf("Hello, %s User!", name))
+	})
+
+	// 経路 + from-data
+	e.PUT("user/:id", func(e echo.Context) error {
+		id := e.Param("id")
+		name := e.FormValue("name")
+		age := e.FormValue("age")
+
+		res := fmt.Sprintf("id:%s, name:%s, age:%s", id, name, age)
+		return e.String(http.StatusOK, res)
+	})
+
+	// query
+	e.GET("/user/search", func(e echo.Context) error {
+		name := e.QueryParam("name")
+		limit := e.Param("limit")
+
+		res := fmt.Sprintf("name:%s, limit:%s", name, limit)
+		return e.String(http.StatusOK, res)
+	})
+
+	// json形式でbinding
+	e.POST("/user", func(e echo.Context) error {
+		var reqDt UserRequest
+		if err := e.Bind(&reqDt); err != nil {
+			return e.JSON(http.StatusBadRequest, err.Error())
+		}
+		res := fmt.Sprintf("name:%s, age:%d", reqDt.UserName, reqDt.UserAge)
+		return e.String(http.StatusOK, res)
+	})
+
+	// bindingを構造体を使わないでできるか確認
+	e.POST("/user", func(e echo.Context) error {
+		var reqDt any
+		if err := e.Bind(&reqDt); err != nil {
+			return e.JSON(http.StatusBadRequest, err.Error())
+		}
+
+		// ひとつづつ取り出したり、項目確認するが面倒くさい
+		// 構造体で管理した方が良さそう
+		name := ""
+		age := 0
+		dt := reqDt.(map[string]any)
+		if v, ok := dt["userName"].(string); ok {
+			name = v
+		}
+		if v, ok := dt["userAge"].(int); ok {
+			age = v
+		}
+		res := fmt.Sprintf("name:%s, age:%d", name, age)
+
+		return e.String(http.StatusOK, res)
+	})
+}
+
+type UserRequest struct {
+	UserName string `json:"userName"`
+	UserAge  int    `json:"userAge"`
 }
