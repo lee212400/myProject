230e0f7334b57f5dfa71557bed4d66e38d8b2db3
lee212400 <i2124@agent-grow.com>
2025-04-08 10:54:47 +0900

    echo-middleware

---
+++ src/echo-middleware/cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	setMiddleware(e)
+	e.Logger.Fatal(e.Start(":8888"))
+}
+
+func setMiddleware(e *echo.Echo) {
+	// rogging + custom
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `${method} ${uri} ${status} ${latency_human}\n`,
+	}))
+
+	e.Use(middleware.Recover()) // panic発生したら、サーバーが落ちないようにする
+
+	// CORS許可
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://sample.com"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost},
+	}))
+
+	// jwt token認証
+	jwtMiddleware := echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte("my-secret"),
+		ContextKey:  "user",
+		TokenLookup: "header:Authorization", // "Authorization: Bearer <token>"
+		KeyFunc:     getKey,
+	})
+
+	e.GET("/user/:id", func(c echo.Context) error {
+		myId := c.Param("id")
+		// JWTから情報抽出
+		user := c.Get("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+		id := claims["id"].(string)
+		if myId != id {
+			return c.JSON(http.StatusUnauthorized, map[string]string{
+				"message": "Unauthorized",
+			})
+		}
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "OK id" + id,
+		})
+	}, jwtMiddleware)
+
+	// api limit設定
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(10)))
+
+	// header request Id挿入
+	e.Use(middleware.RequestID())
+}
+
+// keyfuncをカスタマイズしてjwtのヘッダー,exipre等確認ができる
+func getKey(token *jwt.Token) (interface{}, error) {
+	_, ok := token.Header["kid"].(string)
+	if !ok {
+		return nil, errors.New("expecting JWT header to have a key ID in the kid field")
+	}
+
+	typ, ok := token.Header["typ"].(string)
+	if ok {
+		fmt.Printf("Token type (typ): %s\n", typ)
+	} else {
+		fmt.Println("No 'typ' field in the JWT header")
+	}
+
+	return nil, nil
+}
