667a3b9937736ee039ca3824133fbf69e4032271
lee212400 <i2124@agent-grow.com>
2025-04-09 13:51:53 +0900

    server-stream

--- src/grpc-sample/cmd/main.go
+++ src/grpc-sample/cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net"
+	"sync"
 
 	"google.golang.org/grpc"
 
@@ -15,16 +16,56 @@ type server struct {
 }
 
 func (s *server) GetData(req *pb.StreamRequest, stream pb.SampleService_GetDataServer) error {
-	for i := 0; i <= 100; i++ {
-		res := &pb.StreamResponse{
-			Name:  fmt.Sprintf("name%d", i),
-			Email: fmt.Sprintf("sample%d@test.com", i),
+	ctx := stream.Context()
+	dataCh := make(chan map[string]string, 1)
+	errCh := make(chan error, 1)
+
+	// GetData関連実行
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		data, err := getData(&ctx)
+		if err != nil {
+			errCh <- err
+			return
 		}
-		if err := stream.Send(res); err != nil {
+		dataCh <- data
+
+	}()
+
+	go func() {
+		wg.Wait()
+		close(dataCh)
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("context canceled or timeout:", ctx.Err())
+			return ctx.Err()
+
+		case err := <-errCh:
+			log.Println("getData failed:", err)
 			return err
+
+		case data, ok := <-dataCh:
+			if !ok {
+				log.Println("Data channel closed, ending stream")
+				return nil
+			}
+
+			res := &pb.StreamResponse{
+				Name:  data["name"],
+				Email: data["email"],
+			}
+			if err := stream.Send(res); err != nil {
+				log.Printf("stream send error: %v", err)
+				return err
+			}
 		}
 	}
-	return nil
+
 }
 
 func main() {
@@ -33,4 +74,13 @@ func main() {
 	pb.RegisterSampleServiceServer(s, &server{})
 	log.Println("Server running at :50051")
 	s.Serve(lis)
+}
+
+func getData(ctx *context.Context) (map[string]string, error) {
+	// db,外部API処理
+	res := map[string]string{
+		"eame":  "name",
+		"email": "sample@test.com",
+	}
+	return res, nil
 }
