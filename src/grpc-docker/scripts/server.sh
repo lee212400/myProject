ea681569f0a4dafa58b30a77b38baa4e6a5ac1dc
lee212400 <i2124@agent-grow.com>
2025-04-14 17:07:04 +0900

    docker-grpc

---
+++ src/grpc-docker/scripts/server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
+
+docker build -t go-grpc-server -f "$ROOT_DIR/docker/grpc/Dockerfile" "$ROOT_DIR"
+
+docker run --rm -p 50051:50051 go-grpc-server
