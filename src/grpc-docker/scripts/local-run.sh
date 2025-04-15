0a0e184a14f51e8c927589d839e850526e6f63b0
lee212400 <i2124@agent-grow.com>
2025-04-15 15:36:13 +0900

    docker-compse②

--- src/grpc-docker/scripts/local-run.sh
+++ src/grpc-docker/scripts/local-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -ex
 ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
 
-docker compose -f "$ROOT_DIR/docker-compose/docker-compose.yaml build grpc-server"
-
-docker compose -f "$ROOT_DIR/docker-compose/docker-compose.yaml" up -d
+docker compose -f "$ROOT_DIR/docker-compose/protoc/docker-compose.yaml" \
+    -f "$ROOT_DIR/docker-compose/app/docker-compose.yaml" \
+    up --build
