cefb5bc5fc9663c2eb3c3ec2afff6aac472e0fba
lee212400 <i2124@agent-grow.com>
2025-04-17 13:23:42 +0900

    app docker

--- src/grpc-api/scripts/run/run.sh
+++ src/grpc-api/scripts/run/run.sh
@@ -4,6 +4,10 @@ set -ex
 
 ROOT_DIR=$(cd "$(dirname "$0")/../.." && pwd)
 
+source "$ROOT_DIR/conf/config.conf"
+
 bash "$ROOT_DIR/scripts/mysql/mysql.sh"
 
-go run "$ROOT_DIR/cmd/main.go"
+bash "$ROOT_DIR/scripts/proto/proto.sh"
+
+docker compose -f "$ROOT_DIR/docker-compose/app/docker-compose.yaml" up --build
