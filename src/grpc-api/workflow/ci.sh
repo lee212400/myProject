f7cc1683c04751b882c164a5ea755690b671047d
lee212400 <i2124@agent-grow.com>
2025-04-24 14:57:14 +0900

    script修正

--- src/grpc-api/workflow/ci.sh
+++ src/grpc-api/workflow/ci.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Installing golangci-lint..."
+curl -sSfL https://github.com/golangci/golangci-lint/releases/download/v2.1.2/golangci-lint-2.1.2-linux-amd64.tar.gz | tar -xvzf - -C /usr/local/bin
+
+echo "golangci-lint version:"
+golangci-lint --versio
+
 echo "Running golangci-lint..."
 golangci-lint run ./... -v
