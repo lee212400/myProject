8758daa6fab3b808761a16037ca3606984f26aff
lee212400 <i2124@agent-grow.com>
2025-05-02 17:05:26 +0900

    waitgroup

--- src/grpc-api/goroutines/main.go
+++ src/grpc-api/goroutines/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"sync"
 	"time"
 )
 
@@ -9,7 +12,10 @@ func main() {
 	//runGoroutine()
 	//runGoroutineChan()
 	//runGoroutineSelect()
-	runGoroutine2()
+	//runGoroutine2()
+	//runGoroutineWaitGroup()
+	//runGoroutineWaitGroup2()
+	runGoroutineSample()
 }
 
 func runGoroutine() {
@@ -97,4 +103,113 @@ func printMsg(msg string) {
 		fmt.Println(msg, i)
 		time.Sleep(200 * time.Millisecond)
 	}
+}
+
+func runGoroutineWaitGroup() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1) // goroutine count +1
+
+		go func(n int) {
+			defer wg.Done() // goroutine count -1
+			fmt.Println("goroutine test :", n)
+		}(i)
+	}
+
+	wg.Wait() // goroutineが終了(Done)まで待機
+	fmt.Println("end")
+}
+
+func runGoroutineWaitGroup2() {
+	var (
+		worker   = 5
+		dataChan = make(chan int)
+		result   = []int{}
+		wg       sync.WaitGroup
+	)
+
+	for i := 0; i < worker; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			dataChan <- n
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	for num := range dataChan {
+		result = append(result, num)
+	}
+
+	fmt.Println("result:", result)
+}
+
+const maxWorkers = 5
+
+func runGoroutineSample() {
+	msgQueue := make(chan string)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	//defer close(msgQueue)
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, maxWorkers)
+
+	go func() {
+		for i := 0; i < 20; i++ {
+			msg := fmt.Sprintf("message-%d", i)
+			msgQueue <- msg
+			time.Sleep(100 * time.Millisecond)
+		}
+		close(msgQueue)
+	}()
+
+	go func() {
+		wg.Wait()
+
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("context canceled")
+			return
+		case msg, ok := <-msgQueue:
+			if !ok {
+				log.Println("msgQueue closed")
+				return
+			}
+
+			sem <- struct{}{}
+			wg.Add(1)
+
+			go func(m string) {
+				defer func() {
+					<-sem
+					wg.Done()
+				}()
+
+				if err := handleMessage(m); err != nil {
+					log.Printf("error: %v\n", err)
+				}
+			}(msg)
+		}
+	}
+
+}
+
+func handleMessage(msg string) error {
+	log.Println("start message:", msg)
+
+	if msg == "message-13" {
+		return fmt.Errorf("error message: %s", msg)
+	}
+	log.Println("message:", msg)
+	time.Sleep(500 * time.Millisecond)
+
+	return nil
 }
